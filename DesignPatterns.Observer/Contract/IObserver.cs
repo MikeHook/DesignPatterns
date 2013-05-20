799011ed706484e98f8d641ecf117a10dc9551b9
Mike Hook <hook.mike@gmail.com>
2013-05-20 09:01:47 +0100

    Observer pattern bare classes.

---
+++ DesignPatterns.Observer/Contract/IObserver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Observer.Contract
+{
+	public interface IObserver
+	{
+	}
+}
---
+++ DesignPatterns.Tests/ObserverTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using DesignPatterns.Observer;
+
+namespace DesignPatterns.Tests
+{
+	/// <summary>
+	/// These are tests for the kata implementing the Observer pattern:
+	/// Define a one-to-many relationship between objects 
+	/// so that when one object changes state all it dependants are notified and updated automatically
+	/// </summary>
+	[TestFixture]
+	public class ObserverTests
+	{
+		ElectionData Election;
+
+		[SetUp]
+		public void SetUp()
+		{
+			Election = new ElectionData();			
+		}
+
+	}
+}
